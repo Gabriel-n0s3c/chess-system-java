dad82058fa38242a78e9cbf2717a68440a823802
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2024-01-04 16:31:58 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,10 +58,17 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
-        if(chessMatch.getCheck()){
-            System.out.println("CHECK!");
+
+        if(!chessMatch.getCheckMate()){
+            System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
+            if(chessMatch.getCheck()){
+                System.out.println("CHECK!");
+            }
+        }else{
+            System.out.println("CHECKMATE!");
+            System.out.println("Vencedor: "+ chessMatch.getCurrentPlayer());
         }
+
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
